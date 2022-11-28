545691eb907dacbfd18398e338fd86317133f304
jwseo <jwseo-intern@insilicogen.com>
2022-11-28 14:05:30 +0900

    add static_root

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('bookmark/', include('bookmark.urls')),
     path("admin/", admin.site.urls),
+    path('', include('bookmark.urls'))
 ]
